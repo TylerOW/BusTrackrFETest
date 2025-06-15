643763fe4bf4c640c1c9ba1a953133aacf75027c
TylerOW <123558433+TylerOW@users.noreply.github.com>
2025-06-15 20:30:42 +0800

    Map change

--- app/src/main/java/com/example/bustrackingapp/feature_bus_routes/presentation/bus_routes/BusRoutesScreen.kt
+++ app/src/main/java/com/example/bustrackingapp/feature_bus_routes/presentation/bus_routes/BusRoutesScreen.kt
@@ -1,48 +1,33 @@
 package com.example.bustrackingapp.feature_bus_routes.presentation.bus_routes
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.ContentScale
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.bustrackingapp.R
 import com.example.bustrackingapp.core.presentation.components.CustomLoadingIndicator
 import com.example.bustrackingapp.core.util.LoggerUtil
 import com.example.bustrackingapp.feature_bus_routes.domain.models.BusRouteWithStops
 import com.example.bustrackingapp.feature_bus_routes.presentation.components.BusRouteTile
-import com.example.bustrackingapp.feature_bus_routes.presentation.components.ShuttleRouteMap
 import com.example.bustrackingapp.ui.theme.Blue500
 import com.example.bustrackingapp.ui.theme.NavyBlue300
 import com.example.bustrackingapp.ui.theme.Red400
 import com.example.bustrackingapp.ui.theme.White
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -91,8 +76,7 @@ fun BusRoutesScreen(
                         snackbarData = it,
                         containerColor = Red400,
                         contentColor = White,
-
-                        )
+                    )
                 }else{
                     Snackbar(snackbarData = it)
                 }
@@ -113,7 +97,7 @@ fun BusRoutesScreen(
                 onRouteItemClick = onRouteItemClick
             )
         }
-    } 
+    }
 }
 
 @Composable
@@ -188,10 +172,14 @@ private fun BusRouteList(
 
     Column {
         ShuttleRulesInfo(modifier = Modifier.padding(horizontal = 8.dp))
-        ShuttleRouteMap(
+        Image(
+            painter = painterResource(id = R.drawable.shuttlemapdone),
+            contentDescription = "UNITEN Shuttle Route Map",
             modifier = Modifier
-                .padding(vertical = 8.dp)
-                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .align(Alignment.CenterHorizontally),
+            contentScale = ContentScale.FillWidth
         )
 
         SwipeRefresh(
@@ -217,5 +205,4 @@ private fun BusRouteList(
             )
         }
     }
-
 }
